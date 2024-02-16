84090fdeb1cf5649632a9a444071bb91afbeba5a
hm-huzaifa <mhuzaifa150@gmail.com>
2024-02-16 18:45:51 +0500

    until db...

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -2,11 +2,13 @@ import { Button } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
 import Image from "next/image";
+import {ModeToggle} from "@/components/mode-toggle";
 
 export default function Home() {
   return (
-    <div>
+    <div className="container">
       <UserButton afterSignOutUrl="/" />
+        <ModeToggle />
     </div>
   );
 }
